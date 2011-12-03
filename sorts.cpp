2dc4d32c6fd85f6426f6c9f2be2097fb2fb9f85f
Randall Peterson <petersrk@email.sc.edu>
2011-12-02 19:49:26 -0500

    mergesort completed

--- sorts.cpp
+++ sorts.cpp
@@ -42,9 +42,56 @@ void quickSort(vector<int> input, int left, int right) {
 	}
 }
 
-void mergeSort(vector<int> input) {
-	
-	
+
+vector<int> merge(vector<int>& left, vector<int>& right) {
+	vector<int> result;
+	//reverse elements since popping from the front
+	//isnt supported in vectors. probably should have
+	//used a dequeue instead
+	reverse(left.begin(), left.end());
+	reverse(right.begin(), right.end());
+	while (left.size() > 0 || right.size() > 0) {
+		if (left.size() > 0 && right.size() > 0) {
+			if (left.back() <= right.back()) {
+				result.push_back(left.back());
+				left.pop_back();
+			}
+			else {
+				result.push_back(right.back());
+				right.pop_back();
+			}
+		}
+		else if (left.size() > 0) {
+			result.push_back(left.back());
+			left.pop_back();
+		}
+		else if (right.size() > 0) {
+			result.push_back(right.back());
+			right.pop_back();
+		}
+	}
+	return result;
+}
+
+vector<int>  mergeSort(vector<int> input) {
+	int middle;
+	vector<int> left,right,result;
+	if (input.size() <= 1) {
+		return input;
+	}
+	else {
+		middle = (input.size()-1)/2;
+		for (int i = 0; i < middle; i++) {
+			left.push_back(input[i]);
+		}
+		for (int i = middle; i < input.size(); i++) {
+			right.push_back(input[i]);
+		}
+		left = mergeSort(left);
+		right = mergeSort(right);
+		result = merge(left,right);
+		return result;
+	}
 }
 
 int main(int argc, char *argv[]) {
